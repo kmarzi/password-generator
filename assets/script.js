b590c721b0dae90ea036675e85b4d1c0cb5b2000
Kmarzi <k.l.marziali@gmail.com>
2020-07-31 20:01:35 -0400

    added variables to js

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,16 @@ var generateBtn = document.querySelector("#generate");
 document.getElementById("generate").addEventListener("click", myFunction);
 
 function myFunction() {
-  alert ("Would you like to change your password?");
-  prompt("password needs to contain atleast 1 Uppercase letter,lowercase letter and a number.")
-}
+  confirm("Would you like to change your password?");
+  confirm("Do you want to use letters?")
+  confirm("Do you want to use numbers?")
+  confirm("Do you want to use symbols?")
 
-function generatePassword() {
-  var length = 8,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
-  }
-  return retVal;
+  passwordLength = confirm"Password must contain atleast 8 characters.")
+    if (passwordLength < 8);
+    confirm("Password must contain atleast 8 characters!")
 }
-
+ 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -25,6 +21,12 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+ var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+ var upperCase =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+ var numberVal =['1','2','3','4','5','6','7','8','9','0']
+ var specialVal =['~','!','@','#','$','%','^','&','*','+','=']
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("generate", writePassword);

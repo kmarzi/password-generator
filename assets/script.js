a632792f89c7acbc8633dd8cbc823c7fc1b2189f
Kmarzi <k.l.marziali@gmail.com>
2020-07-31 20:36:48 -0400

    added an array of characters to console.log

--- assets/script.js
+++ assets/script.js
@@ -9,9 +9,8 @@ function myFunction() {
   confirm("Do you want to use numbers?")
   confirm("Do you want to use symbols?")
 
-  passwordLength = confirm"Password must contain atleast 8 characters.")
+  passwordLength = confirm("Password must contain atleast 8 characters.")
     if (passwordLength < 8);
-    confirm("Password must contain atleast 8 characters!")
 }
  
 // Write password to the #password input
@@ -21,11 +20,23 @@ function writePassword() {
 
   passwordText.value = password;
 }
+ var passswordCharacters
+ var passwordLength
 
  var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
  var upperCase =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
  var numberVal =['1','2','3','4','5','6','7','8','9','0']
  var specialVal =['~','!','@','#','$','%','^','&','*','+','=']
+
+   console.log(lowerCase, upperCase, numberVal, specialVal)
+
+ for(var i = 0; i < passwordLength; i++){
+   var randomUpper = Math.floor(Math.random() * lowerCase.length);
+   var randomLower = Math.floor(Math.random() * upperCase.length);
+   var randomNumber = Math.floor(Math.random() * numberVal.length);
+   var randomSpecial = Math.floor(Math.random() * specialVal.length);
+    }
+ 
 
 
 // Add event listener to generate button
